114d82bfd06a1cf61710f98944ac506d6b89bb63
vincemg <vincemg333@gmail.com>
2014-10-15 21:47:00 -0700

    adding experimental changes to form douments

--- forms/formVerification.js
+++ forms/formVerification.js
@@ -1,3 +1,25 @@
-/**
- * @author Vince Gagliardi
- */
+ // (function (){
+	 // var app = angular.module('checkForm', []);
+	 // app.controller('FormController', function(){}
+	 // });
+
+ angular.module('userSignup', [])
+   .controller('FormController', ['$scope', function($scope) {
+     $scope.master = {};
+ 
+     $scope.update = function(user) {
+       $scope.master = angular.copy(user);
+     };
+ 
+     $scope.reset = function() {
+       $scope.user = angular.copy($scope.master);
+     };
+ 
+     $scope.isUnchanged = function(user) {
+       return angular.equals(user, $scope.master);
+     };
+ 
+     $scope.reset();
+   }]);
+  
+
