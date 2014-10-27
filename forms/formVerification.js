9a85700ae0fc45b51d3a062cee3dbab54d2f9142
vincemg <vincemg333@gmail.com>
2014-10-26 23:09:27 -0700

    adding dropdown list fuctionability

--- forms/formVerification.js
+++ forms/formVerification.js
@@ -19,5 +19,91 @@
  
      $scope.reset();
    }]);
-  
+   
+   
 
+  
+function getStates()
+{
+//setting up team to league associations
+var stateName = document.form.league.value;
+
+//MLB
+var sateChosen = new Array("Alabama",
+"Alaska",
+"Arizona",
+"Arkansas",
+"California",
+"Colorado",
+"Connecticut",
+"Delaware",
+"Florida",
+"Georgia",
+"Hawaii",
+"Idaho",
+"Illinois",
+"Indiana",
+"Iowa",
+"Kansas",
+"Kentucky",
+"Louisiana",
+"Maine",
+"Maryland",
+"Massachusetts",
+"Michigan",
+"Minnesota",
+"Mississippi",
+"Missouri",
+"Montana",
+"Nebraska",
+"Nevada",
+"New Hampshire",
+"New Jersey",
+"New Mexico",
+"New York",
+"North Carolina",
+"North Dakota",
+"Ohio",
+"Oklahoma",
+"Oregon",
+"Pennsylvania",
+"Rhode Island",
+"South Carolina",
+"South Dakota",
+"Tennessee",
+"Texas",
+"Utah",
+"Vermont",
+"Virginia",
+"Washington",
+"West Virginia",
+"Wisconsin",
+"Wyoming",
+	
+	
+	
+	
+);
+
+if (leaguename == "MLB")
+			{
+				var teamsel = mlblge;
+				var numteams = mlblge.length;
+				
+				var makeselect1 = "<select name ='TeamName'><br />";
+				makeselect1 += "<option value=''>Select Team</option><br />";
+				for (i=0; i<=numteams; i++)
+					{ var teamnames = mlblge[i];
+					makeselect1 += '<option value="' + teamnames + '">' + teamnames + '</option>';
+					}
+				makeselect1 += '</select>'; 
+				document.getElementById('teamselect').innerHTML = makeselect1;
+				
+			}
+			
+
+function filltext()
+{
+	var selectedteam = document.form.TeamName.value;
+	document.getElementById("teamnametxt").innerHTML = selectedteam;
+}
